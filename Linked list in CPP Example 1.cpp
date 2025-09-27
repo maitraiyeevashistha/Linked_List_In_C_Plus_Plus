df8891b9be500d68c487568973156709982f5381
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-09-27 12:26:14 +0530

    Create Linked list in CPP Example 1.cpp

---
+++ Linked list in CPP Example 1.cpp
@@ -0,0 +1,29 @@
+// EXPERIMENT-17
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+
+    int val;
+    Node* next;
+
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+int main(){
+    Node* n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
+
+
+/*Output :
+20 0
+*/
