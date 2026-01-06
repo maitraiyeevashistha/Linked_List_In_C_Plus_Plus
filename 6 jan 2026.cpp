bfaa3c6a257bb3752cd3e456a46b260e1a2368aa
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2026-01-06 14:36:20 +0530

    Create 6 jan 2026.cpp

---
+++ 6 jan 2026.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    
+    int roll;
+    Node *Next
+    
+    public :
+    
+    Node(int data){                   // Constructor
+        val = data;
+        next = NULL;
+    }
+    
+}
+
+int Node :: GetRoll(){                // getter
+    return roll;
+}
+
+void Node :: SetRoll(int r){          // setter 
+    roll = r
+}
+
+class LinkedList{
+    Node *head
+}
+ void AppendNode(){
+    cout << "enter the your roll number" << endl;
+    cin  >> r;
+    n -> SetRoll(r);
+    
+    if(head == null){
+        head = n;
+    }
+ }
+
+int main() {
+    int r;
+    Node *n = new Node()
+    if(n == null){
+        return
+    }
+    
+    cout << "enter the your roll number" << endl;
+    cin  >> r;
+    n -> SetRoll(r);
+    
+    if(head == null){
+        head = n;
+    }
+    return 0;
+}
+
+
+
+
+
